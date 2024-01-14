54c0888e08569a9bd53778a3a16352dc03e8310e
PanavShah1 <panavshah@Panavs-MacBook-Pro.local>
2024-01-14 08:54:49 +0530

    changes to customer_page

--- customer_page/customer_page_script.js
+++ customer_page/customer_page_script.js
@@ -2,12 +2,13 @@ let doctors_data = [
     {
         'index': 0,
         'name': 'Sam Murray',
+        'image': 'face1.png',
         'phone-no': '2398475619',
         'password': 'redfish123',
         'specialization': 'orthopedic',
         'appointments': [
             {
-                'patient': patients_data[0],
+                // 'patient': patients_data[0],
                 'date-time': '23/2/23 12:00'
             }
         ]
@@ -15,6 +16,7 @@ let doctors_data = [
     {
         'index': 1,
         'name': 'Danial Smith',
+        'image': 'face2.png',
         'phone-no': '3495837293',
         'password': 'iamthebest34',
         'specialization': 'pediatrician',
@@ -26,6 +28,7 @@ let patients_data = [
     {
         'index': 0,
         'name': 'Tom Cruise',
+        'image': 'face3.png',
         'phone-no': '3928374637',
         'password': 'best543',
         'appointments': [{
@@ -34,23 +37,54 @@ let patients_data = [
         }
 
         ]
+    },
+    {
+        'index': 1,
+        'name': 'Will Smith',
+        'image': 'face4.png',
+        'phone-no': '7384756473',
+        'password': 'slap989',
+        'appointments': [
+        ]
     }
 ]
 
+let x = 0;
 
 
 
-x = 0;
+
 
 document.addEventListener('DOMContentLoaded', function() {
     // Your script code here
+
+    // <h2>Your Profile</h2>
+    //             <img src="face1.png" alt="">
+    //             <p>Full Name: name</p>
+    //             <p>Mobile Number: 9876543210</p>
+    let cont = document.querySelector('.profile');
+    let newElement1 = document.createElement('h2');
+    newElement1.innerHTML = 'Your Profile';
+    let newElement2 = document.createElement('img');
+    newElement2.setAttribute('src', patients_data[x]['image']);
+    let newElement3 = document.createElement('p');
+    newElement3.innerHTML = 'Full Name : '+patients_data[x]['name'];
+    let newElement4 = document.createElement('p');
+    newElement4.innerHTML = 'Mobile Number : '+patients_data[x]['phone-no'];
+    cont.appendChild(newElement1);
+    cont.appendChild(newElement2);
+    cont.appendChild(newElement3);
+    cont.appendChild(newElement4);
+
+
+    
     let len = doctors_data.length;
     console.log('len1 '+len);
     for (let i = 0; i<len; i++){
         console.log(doctors_data[i]['name']);
         let newElemtnt = document.createElement('div');
         newElemtnt.classList.add('doctor');
-        newElemtnt.innerHTML = '<img src="face.png" alt=""><div><p>Name : <span class="imp">'+doctors_data[i]['name']+'</span></p><p>Specialization : <span class="imp">'+doctors_data[i]['specialization']+'</span></p></div>'
+        newElemtnt.innerHTML = '<img src='+doctors_data[i]['image']+'><div><p>Name : <span class="imp">'+doctors_data[i]['name']+'</span></p><p>Specialization : <span class="imp">'+doctors_data[i]['specialization']+'</span></p></div>'
         document.querySelector('.right').appendChild(newElemtnt);
 
         let newButton = document.createElement('button');
@@ -69,7 +103,7 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(doctors_data[j]['name']);
         let newElemtnt = document.createElement('div');
         newElemtnt.classList.add('doctor');
-        newElemtnt.innerHTML = '<img src="face.png" alt=""><div><p>Name : <span class="imp">'+patients_data[x]['appointments'][j]['doctor']['name']+'</span></p><p>Specialization : <span class="imp">'+doctors_data[j]['specialization']+'</span></p><br><p>Date/Time: <span class="imp">'+patients_data[x]['appointments'][j]['date-time']+'</span></p></div>'
+        newElemtnt.innerHTML = '<img src='+patients_data[x]['appointments'][j]['doctor']['image']+'><div><p>Name : <span class="imp">'+patients_data[x]['appointments'][j]['doctor']['name']+'</span></p><p>Specialization : <span class="imp">'+doctors_data[j]['specialization']+'</span></p><br><p>Date/Time: <span class="imp">'+patients_data[x]['appointments'][j]['date-time']+'</span></p></div>'
         document.querySelector('.appointments').appendChild(newElemtnt);
     }
 
@@ -102,7 +136,3 @@ function submit(event){
     // event.preventDefault();
 }
 
-function updateAppo(pat, doc){
-    console.log('hello');
-    
-}
